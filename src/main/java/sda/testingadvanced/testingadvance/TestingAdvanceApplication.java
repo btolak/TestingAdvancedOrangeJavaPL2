20307d3e1c28c32b8f2d74bb2fe6a39a0a11fe29
Bartek <bartlomiejtolak7@gmail.com>
2023-05-11 11:06:42 +0200

    init spring repo for testing advance

---
+++ src/main/java/sda/testingadvanced/testingadvance/TestingAdvanceApplication.java
@@ -0,0 +1,13 @@
+package sda.testingadvanced.testingadvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingAdvanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingAdvanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sda/testingadvanced/testingadvance/TestingAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package sda.testingadvanced.testingadvance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
